e97210ef82664c51bb95f88b372ff18bcb9c6b81
JaeHoon Lim <jhlim@fnal.gov>
2016-09-21 22:19:23 +0900

    V1.12

--- CATtoSSB_BatchJob.sh
+++ CATtoSSB_BatchJob.sh
@@ -1,1 +1,1 @@
-create-batch --jobName TT_powheg_pythia8 --fileList $CMSSW_BASE/src/CATTools/CatAnalyzer/data/dataset/dataset_TT_powheg.txt --maxFiles 3000 --cfg CATtoSSB_cfg.py
+create-batch --jobName TT_powheg_pythia8 --fileList $CMSSW_BASE/src/CATTools/CatAnalyzer/data/dataset/dataset_TT_powheg.txt --maxFiles 50 --cfg CATtoSSB_cfg.py
